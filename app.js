8a4ef59085f28f3a568436d65a79c8defea4f299
AyoAfolalu <ayo.afolalu@yahoo.co.uk>
2020-06-21 23:04:44 +0000

    add app file all pages rendering

--- app.js
+++ app.js
@@ -13,6 +13,36 @@ app.get("/",function(req,res)
 		res.render("home");
 		});
 
+app.get("/about",function(req,res)
+{
+		res.render("aboutus");
+		});
+
+app.get("/tenet",function(req,res)
+{
+		res.render("tenet");
+		});
+
+app.get("/biblelessons",function(req,res)
+{
+		res.render("biblelessons");
+		});
+
+app.get("/gallery",function(req,res)
+{
+		res.render("gallery");
+		});
+
+app.get("/contact",function(req,res)
+{
+		res.render("contact");
+		});
+
+app.get("/*",function(req,res)
+{
+		res.render("home");
+		});
+
 
 app.listen(process.env.PORT || 3000, process.env.IP, () =>{
 	console.log('Serv3r has started');
